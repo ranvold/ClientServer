6a7c249badf4ed4f2780958c597f7e914d297917
Volodymyr Shafran <volodymyr.shafran98@gmail.com>
2021-12-04 15:26:07 +0200

    Add project files.

---
+++ SocketClient/Client.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace SocketClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SendMessageFromSocket(1031);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+
+        static void SendMessageFromSocket(int port)
+        {
+            // Буфер для вхідних даних
+            byte[] bytes = new byte[256];
+
+            // З'єднуємося з віддаленим пристроєм
+
+            // Встановлюємо віддалену точку для сокету
+            IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, port);
+
+            Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            // З'єднуємо сокет із віддаленою точкою
+            sender.Connect(ipEndPoint);
+
+            Console.Write("Enter command: \n\"Who\" - to get the author" +
+                " \n\"0\" - to get the entire list of server directories" +
+                " \n\"1\" - get a filtered list, you will be asked to enter a filter\n");
+            string message = Console.ReadLine();
+            SaveClientLog(message);
+
+            Console.WriteLine("The socket connects to {0} ", sender.RemoteEndPoint.ToString());
+            byte[] msg = Encoding.UTF8.GetBytes(message);
+
+            // Надсилаємо дані через сокет
+            int bytesSent = sender.Send(msg);
+
+            if (message == "1")
+            {
+                Console.Write("Enter filter: ");
+                message = Console.ReadLine();
+                SaveClientLog(message);
+                msg = Encoding.UTF8.GetBytes(message);
+                bytesSent = sender.Send(msg);
+            }
+
+            // Отримуємо відповідь від сервера
+            int bytesRec = sender.Receive(bytes);
+
+            message = Encoding.UTF8.GetString(bytes, 0, bytesRec);
+            SaveClientLog(message);
+            Console.WriteLine("\nServer response: {0}\n\n", message);
+
+            // Використовуємо рекурсію для неодноразового виклику SendMessageFromSocket()
+            if (message.IndexOf("<TheEnd>") == -1)
+                SendMessageFromSocket(port);
+
+            // Звільняємо сокет
+            sender.Shutdown(SocketShutdown.Both);
+            sender.Close();
+        }
+        public static void SaveClientLog(string socket)
+        {
+            string writePath = @"C:\Users\rovald\source\repos\ClientServer\SocketClient\ClientLog.txt";
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine(DateTime.Now + $" {socket}");
+                    sw.WriteLine();
+                    sw.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ SocketServer/Server.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace SocketServer
+{
+    class Program
+    {
+        private static List<string> directory = new(){ 
+            "lab1_AOC_Volodymyr_Shafran_K25.cs",
+            "Lab1_OOP",
+            "list_of_debts.txt",
+            "hw6_OOP.cs",
+            "record12_math_log.mp4",
+            "essay_OOP.docx",
+            "lab2_AOC_test.docx",
+            "hw7_OOP.cs"
+        };
+        static void Main(string[] args)
+        {
+            // Встановлюємо для сокету локальну кінцеву точку
+            IPHostEntry ipHost = Dns.GetHostEntry("localhost");
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 1031);
+
+            // Створюэмо сокет Tcp/Ip
+            Socket sListener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            // Призначаємо сокет локальної кінцевої точки та слухаємо вхідні сокети
+            try
+            {
+                sListener.Bind(ipEndPoint);
+                sListener.Listen(10);
+
+                // Починаємо слухати з'єднання
+                while (true)
+                {
+                    Console.WriteLine("Waiting for a connection through the port {0}", ipEndPoint);
+
+                    // Програма припиняється, очікуючи на вхідне з'єднання
+                    Socket handler = sListener.Accept();
+                    string data = null;
+
+                    // Ми дочекалися клієнта, який намагається з нами з'єднатися
+                    byte[] bytes = new byte[256];
+                    int bytesRec = handler.Receive(bytes);
+
+                    data = Encoding.UTF8.GetString(bytes, 0, bytesRec);
+                    SaveServerLog(data);
+
+                    // Показуємо дані на консолі
+                    Console.Write("Received text: " + data + "\n\n");
+
+                    // Надсилаємо відповідь клієнту
+                    string reply = "";
+                    if (data == "Who")
+                    {
+                        reply = "Shafran Volodymyr K25 Var-6";
+                    }
+                    else if (data == "0")
+                    {
+                        reply = GetAll();
+                    }
+                    else if (data == "1")
+                    {
+                        bytesRec = handler.Receive(bytes);
+                        data = Encoding.UTF8.GetString(bytes, 0, bytesRec);
+                        SaveServerLog(data);
+                        reply = GetWithFilter(data);
+                    }
+                    else
+                    {
+                        reply = "Unknown command!";
+                    }
+
+                    SaveServerLog(reply);
+                    byte[] msg = Encoding.UTF8.GetBytes(reply);
+                    handler.Send(msg);
+
+
+                    if (data.IndexOf("<TheEnd>") > -1)
+                    {
+                        Console.WriteLine("The server has ended the connection with the client.");
+                        break;
+                    }
+
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+        public static string GetAll()
+        {
+            string reply = "";
+            foreach (var item in directory)
+            {
+                reply += "\n" + item;
+            }
+            return reply;
+        }
+
+        // Використовуэмо регулярні вирази для фільтрації
+        public static string GetWithFilter(string filter)
+        {
+            string reply = "";
+            Regex rgx = new Regex(filter);
+            foreach (var item in directory)
+            {
+                if (rgx.IsMatch(item))
+                {
+                    reply += "\n" + item;
+                }
+            }
+            if (reply == "")
+            {
+                reply = "Not found";
+            }
+            return reply;
+        }
+        public static void SaveServerLog(string socket)
+        {
+            string writePath = @"C:\Users\rovald\source\repos\ClientServer\SocketServer\ServerLog.txt";
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine(DateTime.Now + $" {socket}");
+                    sw.WriteLine();
+                    sw.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
